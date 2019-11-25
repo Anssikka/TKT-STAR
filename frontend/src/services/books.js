0899788d0b39ec7e1ea3f3a1d5ceb06aafc7fb5a
Joakim <joakim.joensuu@gmail.com>
2019-11-25 18:43:30 +0200

    Changed baseurl

--- frontend/src/services/books.js
+++ frontend/src/services/books.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://127.0.0.1:5000/api/books'
+const baseUrl = 'http://127.0.0.1:5000/api/recommendations'
 
 const createBook = async book => {
   const response = await axios.post(baseUrl, book)
