b386c7fcf42862ddc778557f8528cb171b18f8f6
Joakim <joakim.joensuu@gmail.com>
2019-11-25 18:20:31 +0200

    Modified test assert

--- backend/application/tests/test_database.py
+++ backend/application/tests/test_database.py
@@ -25,7 +25,7 @@ def client():
 def test_initialized_db(client):
     rv = client.get('/')
 
-    assert b'' in rv.data
+    assert b'Clean Code: A Handbook of Agile Software Craftsmanship' in rv.data
 
 
 def test_get_after_post(client):
