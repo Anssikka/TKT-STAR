77244b26010a00e0cc0101fd259cffe701500c98
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-11-26 12:36:31 +0200

    Fixed built_frontend

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PROJECT_ROOT=$(pwd)
 
-cd backend/application/ && rm -rf static/
+cd backend/application/ && rm -rf built_frontend/
 cd ../../frontend/ && rm -rf build/
-npm run build && cp -r build/ ../backend/application/ && cd ../backend/application/ && mv build/ static/ && cd $PROJECT_ROOT
+npm run build && cp -r build/ ../backend/application/ && cd ../backend/application/ && mv build/ built_frontend/ && cd $PROJECT_ROOT
 cd backend/ && python3 run.py
