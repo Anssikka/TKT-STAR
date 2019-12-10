24d03dcfdbae12ee50f6cf27c6457288c0bba276
Valtteri Kodisto <42912038+valtterikodisto@users.noreply.github.com>
2019-12-10 16:24:54 +0200

    Added tests for form tags

--- frontend/src/__tests__/books/AddBookForm.test.js
+++ frontend/src/__tests__/books/AddBookForm.test.js
@@ -7,17 +7,27 @@ afterEach(cleanup)
 
 describe('<AddBookForm />', () => {
   it('sends correct data when submitted', async () => {
-    const book = { title: 'Book Title', author: 'Author', isbn: '1234-4321' }
+    const book = {
+      title: 'Book Title',
+      author: 'Author',
+      isbn: '1234-4321',
+      tags: ['testing', '#1testing']
+    }
+    const tagString = '   testing,  #1testing '
     const handleSubmit = jest.fn().mockResolvedValue(book)
-    const { component, authorInput, titleInput, isbnInput, submit } = await setup(handleSubmit)
+    const { component, authorInput, titleInput, isbnInput, tagInput, submit } = await setup(
+      handleSubmit
+    )
 
     fireEvent.change(authorInput, { target: { value: book.author } })
     fireEvent.change(titleInput, { target: { value: book.title } })
     fireEvent.change(isbnInput, { target: { value: book.isbn } })
+    fireEvent.change(tagInput, { target: { value: tagString } })
 
     await waitForElement(() => component.getByDisplayValue(book.author))
     await waitForElement(() => component.getByDisplayValue(book.title))
     await waitForElement(() => component.getByDisplayValue(book.isbn))
+    await waitForElement(() => component.getByDisplayValue(tagString))
 
     fireEvent.click(submit)
 
@@ -31,12 +41,14 @@ const setup = async handleSubmit => {
   const authorInput = await component.findByTestId('add-book-author')
   const titleInput = await component.findByTestId('add-book-title')
   const isbnInput = await component.findByTestId('add-book-isbn')
+  const tagInput = await component.findByTestId('add-book-tag')
   const submit = await component.findByTestId('add-book-submit')
   return {
     component,
     authorInput,
     titleInput,
     isbnInput,
+    tagInput,
     submit
   }
 }
