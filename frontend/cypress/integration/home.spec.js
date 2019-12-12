41e6cb08d0aa93103ca4366716080a3cd33469af
Joakim Joensuu <joakim.joensuu@gmail.com>
2019-12-12 18:09:30 +0200

    Cypress tests (#49)
    * Cypress tests
    
    * Deleted build files

--- frontend/cypress/integration/home.spec.js
+++
@@ -1,8 +0,0 @@
-const baseUrl = 'http://localhost:5000'
-
-describe('Home', function() {
-  it('can be opened', function() {
-    cy.visit(baseUrl)
-    cy.contains('Front page')
-  })
-})
--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ PROJECT_ROOT=$(pwd)
 cd backend/application/ && rm -rf built_frontend/
 cd ../../frontend/ && rm -rf build/
 npm run build && cp -r build/ ../backend/application/ && cd ../backend/application/ && mv build/ built_frontend/ && cd $PROJECT_ROOT
-cd backend/ && python3 run.py
+cd backend/ && PYTHON_ENV=TEST python3 run.py
